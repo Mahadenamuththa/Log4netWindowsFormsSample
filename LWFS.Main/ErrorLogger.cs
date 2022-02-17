3b7f75d23aa420b1dc68e6843e2ec510523f7569
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2022-02-18 01:33:43 +0530

    add ErrorLogger.cs

---
+++ LWFS.Main/ErrorLogger.cs
@@ -0,0 +1,58 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace LWFS.Main
+{
+    public static class ErrorLogger
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(ErrorLogger));
+        #region Constructor
+        static ErrorLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+        #endregion
+
+        #region Public Methods
+        public static bool AddError(Exception ex)
+        {
+            if (ex != null)
+            {
+                StackTrace exTrace = new StackTrace(ex, true);
+                // Class name
+                string className = string.Format("Class name: {0}", exTrace.GetFrame(exTrace.FrameCount - 1).GetMethod().ReflectedType.Name);
+                // Method name
+                string methodName = string.Empty;
+                if (exTrace.GetFrame(exTrace.FrameCount - 1).GetMethod().MemberType == MemberTypes.Method)
+                {
+                    methodName = string.Format("Method name: {0}", exTrace.GetFrame(exTrace.FrameCount - 1).GetMethod().Name);
+                }
+                else
+                {
+                    methodName = string.Format("Method name: {0}", exTrace.GetFrame(exTrace.FrameCount - 1).GetMethod().MemberType.ToString());
+                }
+                // Line number
+                string lineNumber = string.Format("Line number: {0}", exTrace.GetFrame(exTrace.FrameCount - 1).GetFileLineNumber());
+                // Exception message
+                string exception = string.Format("Exception message: {0}", ex.Message + Environment.NewLine + ex.StackTrace);
+                // Inner exception message
+                string innerException = ex.InnerException != null ? Environment.NewLine + string.Format("Inner exception: {0}", ex.InnerException) : string.Empty;
+
+
+                string errorDetails = Environment.NewLine + className
+                                    + Environment.NewLine + methodName
+                                    + Environment.NewLine + lineNumber
+                                    + Environment.NewLine + exception
+                                    + innerException
+                                    + Environment.NewLine;
+
+                logger.Error(errorDetails);
+            }
+            return true;
+        }
+        #endregion
+    }
+}
